428c0d2cb513b35facb3f7d6ad624b71a92cdb70
tundeakins <babatundeakinsanmi@gmail.com>
2021-12-09 09:24:27 +0000

    Remove the now ignored directory and files

--- build/lib/CONAN3/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ["load_lightcurves", "load_rvs", "setup_fit", "fit_data", "create_configfile"]
-from .__version__ import __version__ 
-from ._classes import load_lightcurves, load_rvs, setup_fit, create_configfile, load_chains
-from .fit_data import fit_data
